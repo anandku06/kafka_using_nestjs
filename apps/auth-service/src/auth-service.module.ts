23c26223e0c0ca5a03b75271fcfc047ff052893f
Anand Kumar <rishavgupta163@gmail.com>
2026-06-20 14:03:14 +0530

    Implement Kafka integration with user registration and add Kafka module structure

--- apps/auth-service/src/auth-service.module.ts
+++ apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
+import { KafkaModule } from '@app/kafka';
+import { SERVICES } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [KafkaModule.register(`${SERVICES.AUTH_SERVICE}-group`)],
   controllers: [AuthServiceController],
   providers: [AuthServiceService],
 })
--- apps/auth-service/src/auth-service.service.ts
+++ apps/auth-service/src/auth-service.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { KAFKA_SERVICE, KAFKA_TOPICS } from '@app/kafka';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class AuthServiceService {
+export class AuthServiceService implements OnModuleInit {
+  constructor(
+    @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    // connect to kafka
+    await this.kafkaClient.connect();
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello world!';
+  }
+
+  async simulateUserRegistration(email: string) {
+    // publish event to kafka
+    await this.kafkaClient.emit(KAFKA_TOPICS.USER_REGISTERED, {
+      email,
+      timestamp: new Date().toISOString(),
+    });
+
+    return { message: `User registered: ${email}` };
   }
 }
