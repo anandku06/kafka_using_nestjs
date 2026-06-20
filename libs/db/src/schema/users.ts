9f0a553ee858513b842bd949c2f6a6a2c7643b36
Anand Kumar <rishavgupta163@gmail.com>
2026-06-20 19:24:37 +0530

    feat(db): add database module and service with schema definition

---
+++ libs/db/src/schema/users.ts
@@ -0,0 +1,19 @@
+import { timestamp } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+
+// Enums
+export const roleEnum = pgEnum('role', ['USER', 'ADMIN', 'ORGANIZER']);
+
+// Users
+export const users = pgTable('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  email: varchar('email', { length: 255 }).notNull(),
+  password: varchar('password', { length: 10 }).notNull(),
+  name: varchar('name', { length: 255 }).notNull(),
+  role: roleEnum('role').default('USER').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
