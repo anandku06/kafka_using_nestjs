7de680b614677a6670a77d9babf2555299a1bcb3
Anand Kumar <rishavgupta163@gmail.com>
2026-06-20 12:45:00 +0530

    Refactor AppService to return API Gateway port and add common library structure with interfaces and utilities

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,11 @@
 export * from './common.module';
 export * from './common.service';
+
+// constants
+export * from './constants';
+
+// interfaces
+export * from './interfaces';
+
+// utils
+export * from './utils';
---
+++ libs/common/src/interfaces/user.interface.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IAuthUser extends Pick<IUser, 'id' | 'email'> {
+  roles: string[];
+}
+
+
