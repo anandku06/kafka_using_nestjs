919d05423cc5ef7c3756ef0bf2a116fc1d9b12d1
Anand Kumar <rishavgupta163@gmail.com>
2026-06-27 01:08:51 +0530

    feat(auth): add validation pipes and DTOs for user registration and login

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthServiceModule } from './auth-service.module';
+import { SERVICE_PORTS } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthServiceModule);
-  await app.listen(process.env.port ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(SERVICE_PORTS.AUTH_SERVICE);
+  console.log(`Auth service is running in port ${SERVICE_PORTS.AUTH_SERVICE}`);
 }
 bootstrap();
